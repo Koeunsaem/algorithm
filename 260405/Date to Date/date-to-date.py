707d1d173899579e45e199c67f9de2ff04eb5b5d
Eunsaem Ko <129739010+Koeunsaem@users.noreply.github.com>
2026-04-05 00:04:28 +0900

    Title: Date to Date, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/Date to Date/date-to-date.py
+++ 260405/Date to Date/date-to-date.py
@@ -3,15 +3,14 @@ m1, d1, m2, d2 = map(int, input().split())
 # Please write your code here.
 month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-answer = 0
-for i in range(1, m2+1):
-    answer += month[i]
-answer += d2
+def num_of_days(m, d):
+    total_days = 0
 
-for i in range(1, m1+1):
-    answer -= month[i]
-answer -= d1
+    for i in range(1, m):
+        total_days += month[i]
+    total_days += d
 
-if answer == 0:
-    answer += 1
+    return total_days
+
+answer = num_of_days(m2, d2) - num_of_days(m1, d1) + 1
 print(answer)
