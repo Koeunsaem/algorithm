40407696ae6ffa83d7b2cd5799ce9009f6ef0cbc
Eunsaem Ko <129739010+Koeunsaem@users.noreply.github.com>
2026-04-04 23:58:52 +0900

    Title: Date to Date, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260404/Date to Date/date-to-date.py
@@ -0,0 +1,15 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+answer = 0
+for i in range(1, m2+1):
+    answer += month[i]
+answer += d2
+
+for i in range(1, m1+1):
+    answer -= month[i]
+answer -= d1
+
+print(answer)
